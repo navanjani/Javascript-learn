1e54ad0f381caf20e19bd1af4d09669d0d284429
navanjani <navanjani@outlook.com>
2022-06-19 10:25:42 +0200

    BMI cal update

--- index.js
+++ index.js
@@ -1,7 +1,51 @@
+
+if (process.argv.length !== 7) {
+    console.log(`
+      
+      Please provide 5 arguments for
+      
+      weight (kg), 
+      height (m), 
+      age, 
+      do you exercise daily (yes or no)
+      and your gender (m or f)
+      
+      Example:
+  
+      $ node index.js 69 1.60 30 yes f
+    `);
+  
+    process.exit();
+  }
+  
 const weightInKg = parseInt(process.argv[2]);
 const heightInM = parseFloat(process.argv[3]);
 const age = parseInt(process.argv[4]);
+const dailyExercise = process.argv[5]; 
+const gender = process.argv[6] ;
 
+if (isNaN(weightInKg) || isNaN(heightInM) || isNaN(age)) {
+    console.log(` Please make sure weight , height and age are numbers. 
+    `);
+    process.exit();
+}
+
+if (age < 20 ) {
+    console.log(
+        `This BMI calculator is designed for people older than 20`
+    );
+    process.exit();
+}
+
+if (weightInKg < 30 || weightInKg > 300 ){
+    console.log(` Please enter a weight in kgs ,in between 30kg  to 300kg
+    `);
+    process.exit();
+}
+if (dailyExercise !== "yes" && dailyExercise !== "no"){
+    console.log(`Please fiilin the field 'do you exercise daily' with yes or no`);
+    process.exit();
+}
 
 //BMI = weight (kg) / (height (m) x height (m))
 
@@ -12,22 +56,35 @@ const BMI = weightInKg / (heightInM * heightInM);
 const idealWeight = 22.5 * heightInM * heightInM;
 
 // BMR = 10 x weight (kg) + 6.25 x height (cm) - 5 x age
-// Daily calorie usage BMR * 1.4
+// Daily calorie usage = BMR * 1.4
+// with workouts daly calorie usage = BMR * 1.6
 
-const BMR = (10 * weightInKg) + (6.25 * (heightInM *100) - (5 * age));
+let BMR ;
+if ( gender === "m") {
+    BMR = (10 * weightInKg) + (6.25 * (heightInM *100) - (5 * age)) + 50 ;
+}else {
+    BMR = ((10 * weightInKg) + (6.25 * (heightInM *100) - (5 * age)) + 50 ) - 150
+};
 
-const dailyCalorieUsage = BMR * 1.4;
+
+const dailyCalorieUsage = dailyExercise === "yes" ? BMR * 1.6 : BMR * 1.4 ;
+
 
 // weight to lose to reach idealweight = weight (kg) - ideal weight (kg)
 // time (weeks) it will take to reach your ideal weight =amount of weight to lose / 0.5
 // calories consume while dieting = your daily usage - 500
 
-const weightTolose = weightInKg - idealWeight;
+const weightToloseKg = weightInKg - idealWeight;
 
-const timeToReachIdealWeight = weightTolose / 0.5;
+const timeToReachIdealWeight = Math.abs(weightToloseKg / 0.5);
 
-const dailyCalorieConsumption = dailyCalorieUsage - 500;
+let dailyCalorieConsumption ;
 
+if (weightToloseKg < 0 ){
+   dailyCalorieConsumption =  dailyCalorieUsage + 500;
+}else{
+    dailyCalorieConsumption = dailyCalorieUsage - 500;
+}
 
 
 console.log(`
@@ -37,6 +94,9 @@ console.log(`
     
 height : ${heightInM}
 weight : ${weightInKg}
+age : ${age}
+do you exercise daily ? ${dailyExercise}
+gender : ${gender}
 
 
 ********************
@@ -49,8 +109,9 @@ A BMI under 18.5 is considered underweight
 
 A BMI above 25 is considered overweight
 
-Your idaeal weight is ${Math.round(idealWeight)} kg
-With a normal lifestyle you burn ${Math.round(dailyCalorieUsage)}
+Your ideal weight is ${Math.round(idealWeight)} kg
+With your lifestyle you burn ${Math.round(dailyCalorieUsage)} a day
+
 
 
 ********************
@@ -60,5 +121,7 @@ With a normal lifestyle you burn ${Math.round(dailyCalorieUsage)}
 If you want to reach your idael weight of ${Math.round(idealWeight)} kg :
 
 Eat ${Math.round(dailyCalorieConsumption)} calories a day  
-for ${Math.round(timeToReachIdealWeight)} weeks
+for ${Math.round (timeToReachIdealWeight)} weeks
+
+
 `   );
